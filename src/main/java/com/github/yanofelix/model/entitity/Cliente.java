a1050d21ab8ea04a555b312804cf5b3ff3713016
Yan Felix <devyanfelix@gmail.com>
2021-05-26 23:20:19 -0300

    maven fixed

--- src/main/java/com/github/yanofelix/model/entitity/Cliente.java
+++ src/main/java/com/github/yanofelix/model/entitity/Cliente.java
@@ -8,6 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -22,9 +26,12 @@ public class Cliente {
 	private Long id;
 	
 	@Column(nullable = false, length = 60)
+	@NotEmpty
 	private String nome;
 	
 	@Column(nullable = false, length = 11)
+	@NotNull
+	@CPF
 	private String cpf;
 	
 	@Column(name = "data_cadastro", updatable = false)
